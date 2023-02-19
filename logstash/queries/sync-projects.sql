a57a3954f25272523959bb27596886e3871c4efa
PSNAppZ <thepsnarayanan@gmail.com>
2023-02-19 09:02:35 +0530

    Update query to include user details

--- logstash/queries/sync-projects.sql
+++ logstash/queries/sync-projects.sql
@@ -4,8 +4,12 @@ SELECT pl.id,
        p.id,
        p.name,
        p.slug,
-       p.description
+       p.description,
+       p.created_at,
+       u.id as user_id,
+       u.name as user_name
 FROM project_logs pl
-         LEFT JOIN projects p
-                   ON p.id = pl.project_id
+        LEFT JOIN projects p ON p.id = pl.project_id
+        LEFT JOIN user_projects up ON p.id = up.project_id
+        LEFT JOIN users u ON up.user_id = u.id
 WHERE pl.id > :sql_last_value ORDER BY pl.id;
