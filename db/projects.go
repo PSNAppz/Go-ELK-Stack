6fa026babee84aa3d5c05d8736e29dc336dc8275
PSNAppZ <thepsnarayanan@gmail.com>
2023-02-19 09:03:42 +0530

    Update project queries
    Changes
    - Create project with user_id
    - Added GetProjectsByUserId

--- db/projects.go
+++ db/projects.go
@@ -6,18 +6,27 @@ import (
 	"github.com/PSNAppz/Fold-ELK/models"
 )
 
-func (db Database) CreateProject(project *models.Project) error {
-	var id int
+func (db Database) CreateProject(project *models.Project, user *models.User) error {
+	var projectId int
 	query := `INSERT INTO projects(name, slug, description) VALUES ($1, $2, $3) RETURNING id`
-	err := db.Conn.QueryRow(query, project.Name, project.Slug, project.Description).Scan(&id)
+	err := db.Conn.QueryRow(query, project.Name, project.Slug, project.Description).Scan(&projectId)
 	if err != nil {
 		return err
+	}
+
+	// Insert project-user association
+	if user != nil {
+		userProjectQuery := `INSERT INTO user_projects(project_id, user_id) VALUES ($1, $2)`
+		_, err = db.Conn.Exec(userProjectQuery, projectId, user.ID)
+		if err != nil {
+			return err
+		}
 	}
 
 	// log the operation for logstash to pick up and send to elasticsearch
 	// Here we are doing this at app level.
 	logQuery := `INSERT INTO project_logs(project_id, operation) VALUES ($1, $2)`
-	project.ID = id
+	project.ID = projectId
 	_, err = db.Conn.Exec(logQuery, project.ID, insertOp)
 	if err != nil {
 		db.Logger.Err(err).Msg("could not log operation for logstash")
@@ -61,7 +70,7 @@ func (db Database) DeleteProject(projectId int) error {
 
 func (db Database) GetProjectById(projectId int) (models.Project, error) {
 	project := models.Project{}
-	query := "SELECT id, name, slug, descriotion FROM projects WHERE id = $1"
+	query := "SELECT id, name, slug, description FROM projects WHERE id = $1"
 	row := db.Conn.QueryRow(query, projectId)
 	switch err := row.Scan(&project.ID, &project.Name, &project.Slug, &project.Description); err {
 	case sql.ErrNoRows:
@@ -69,6 +78,25 @@ func (db Database) GetProjectById(projectId int) (models.Project, error) {
 	default:
 		return project, err
 	}
+}
+
+// Get all projects by a user
+func (db Database) GetProjectsByUserId(userId int) ([]models.Project, error) {
+	var list []models.Project
+	query := "SELECT id, name, slug, description FROM projects WHERE id IN (SELECT project_id FROM user_projects WHERE user_id = $1)"
+	rows, err := db.Conn.Query(query, userId)
+	if err != nil {
+		return list, err
+	}
+	for rows.Next() {
+		var project models.Project
+		err := rows.Scan(&project.ID, &project.Name, &project.Slug, &project.Description)
+		if err != nil {
+			return list, err
+		}
+		list = append(list, project)
+	}
+	return list, nil
 }
 
 func (db Database) GetProjects() ([]models.Project, error) {
